680a77a2dab858a540d4e620ad5246fff6135447
Malvo <chd_wql@qq.com>
2019-06-05 16:28:48 +0800

    WQl

--- funFourierHuaDong.m
+++ funFourierHuaDong.m
@@ -12,7 +12,7 @@ end
 %基波拟合计算
 mj=1;   %拟合阶数
 nh_y=data(1:365);
-y_fit=funFourier(nh_y,mj);      %调用函数计算拟合结果
+y_fit=funFourier(nh_y,mj);      %调用函数计算拟合结果 y_fit：拟合结果
 tj=y_fit(365)-fs_y(365);
 y_fit2=y_fit-tj;
 fs_y=fs_y';
---
+++ removing_by_fft.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from numpy import pi, sin, cos
+
+
+def harmonic_analysis(df, order):
+    df.fillna(method='bfill', inplace=True)
+    n = df.shape[0]
+    df['IND'] = range(1, n + 1)
+    df['FITTING'] = df['OBSVALUE'].mean()
+    for i in range(1, order):
+        ak = sum(df['OBSVALUE'] * cos(2.0 * pi * df['IND'] / n * i)) / n * 2.0
+        bk = sum(df['OBSVALUE'] * sin(2.0 * pi * df['IND'] / n * i)) / n * 2.0
+        df['FITTING'] = df['FITTING'] \
+                        + ak * cos(2.0 * pi * df['IND'] * i / n) \
+                        + bk * sin(2.0 * pi * df['IND'] * i / n)
+    return df
+
+
+def my_harmonic(df):
+    T = 365
+    c = []
+    for i in range(T):
+        c.append(cos(2 * pi * (i + 1) / T))
+
+    df['OBSVALUE'] * c
+
+
+def rolling_fft(df):
+    df.rolling(window=365, on='OBSVALUE').apply()
